1a8786fd31c3b7432e8ea85f27f5af823cee6ef3
Aditya <adityanagare09@gmail.com>
2023-06-16 19:15:21 +0530

    Font changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { PT_Sans_Narrow } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const narrow = PT_Sans_Narrow({weight : '400',  subsets: ['latin']})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={narrow.className}>{children}</body>
     </html>
   )
 }
